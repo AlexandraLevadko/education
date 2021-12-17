609483de8925f62508697225e5881d4e13f40beb
Левадко Николай <nickros09@yandex.ru>
2021-12-17 19:58:07 +0300

    lesson-3 not finish

--- geek_php/public/index.php
+++ geek_php/public/index.php
@@ -1,9 +1,50 @@
 <?php
 
-use App\Kernel;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+$loader = new FilesystemLoader('../templates');
+$twig = new Environment($loader);
+
+$images = [
+    [
+        'name' => 'image_1',
+        'path' => '../images/image_1.jpg'
+    ],
+    [
+        'name' => 'image_2',
+        'path' => '../images/image_2.jpg'
+    ],
+    [
+        'name' => 'image_3',
+        'path' => '../images/image_3.jpg'
+    ],
+    [
+        'name' => 'image_4',
+        'path' => '../images/image_4.jpg'
+    ],
+    [
+        'name' => 'image_5',
+        'path' => '../images/image_5.jpg'
+    ],
+    [
+        'name' => 'image_6',
+        'path' => '../images/image_6.jpg'
+    ],
+    [
+        'name' => 'image_7',
+        'path' => '../images/image_7.jpg'
+    ],
+];
+
+try {
+    $template = $twig->load('index.html.twig');
+    $list = $template->render([
+        'images' => $images
+    ]);
+    echo $list;
+} catch (Exception $e) {
+    throw new Exception($e->getMessage());
+}
